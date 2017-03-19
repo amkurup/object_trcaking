9110376d982bb7774058921d96412415cbdb1d27
Prithvi Kambhampati <pkambham@mtu.edu>
2017-03-19 09:56:50 -0400

    plus radius for forward and backward motion

--- catkin_ws/src/object_tracking/src/scripts/tracker_proto.py
+++ catkin_ws/src/object_tracking/src/scripts/tracker_proto.py
@@ -7,16 +7,16 @@ import argparse
 import numpy as np
 from   sensor_msgs.msg import Image
 from   collections     import deque
-from object_tracking.msg import location
+from object_tracking.msg import position
 
 def talker():
-    pub = rospy.Publisher('custom_chatter', location, queue_size=10)
+    pub = rospy.Publisher('custom_chatter', position, queue_size=10)
     #rospy.init_node('custom_talker', anonymous=True)
     #r = rospy.Rate(10) 
-    msg = location()
-    msg.x = dX
-    msg.y = dY
-
+    msg = position()
+    msg.x = x
+    msg.y = y
+    msg.radius = radius
     #while not rospy.is_shutdown():
     rospy.loginfo(msg)
     pub.publish(msg)
@@ -26,6 +26,10 @@ counter = 0
 (dX, dY) = (0, 0)
 direction = ""
 pts = deque(maxlen=32)
+radius = 0
+x = 0
+y = 0
+
 class Tracker:
     def __init__(self):
         self.bridge = cv_bridge.CvBridge()
@@ -39,6 +43,9 @@ class Tracker:
 	global dY
 	global direction
 	global pts
+	global radius
+	global x
+	global y
         image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
         yellowLower = np.array([20, 100, 100], np.uint8)
@@ -64,7 +71,7 @@ class Tracker:
                     (0, 255, 255), 2)
 		cv2.circle(image, center, 5, (0, 0, 255), -1)
 		pts.appendleft(center)
-
+		talker()
        	for i in np.arange(1, len(pts)):
 	    if pts[i - 1] is None or pts[i] is None:
 	        continue
@@ -72,7 +79,7 @@ class Tracker:
 	    if counter >= 10 and i == 1 and pts[-10] is not None:
 	        dX = pts[-10][0] - pts[i][0]
 	        dY = pts[-10][1] - pts[i][1]
-		talker()
+		#talker()
 	        (dirX, dirY) = ("", "")
 	        if np.abs(dX) > 20:
 	            dirX = "East" if np.sign(dX) == 1 else "West"
@@ -88,7 +95,7 @@ class Tracker:
 
 	cv2.putText(image, direction, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 
 	    0.65, (0, 0, 255), 3)
-	cv2.putText(image, "dx: {}, dy: {}".format(dX, dY), 
+	cv2.putText(image, "dx: {}, dy: {}, rad: {}".format(dX, dY, radius), 
             (10, image.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 
             0.35, (0, 0, 255), 1)
 
