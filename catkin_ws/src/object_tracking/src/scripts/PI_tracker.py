0e1710502e1b7d8ad1b63ca65faf3e53f1e75ba5
ajayapra <ajayapra@mtu.edu>
2017-03-18 22:29:16 -0700

    Rough P controller for tracking

--- catkin_ws/src/object_tracking/src/scripts/PI_tracker.py
+++ catkin_ws/src/object_tracking/src/scripts/PI_tracker.py
@@ -1,17 +1,40 @@
+#!/usr/bin/env python
 import rospy
 import std_msgs
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Vector3
+from object_tracking.msg import location
+
+global ang_control
+ang_control = 0.0
 
 def callback(data):
-    global
+    global ang_control
+    reference = 0
+    P = 0.1
+    ang_control = P*(reference - data.x)
+    #rospy.loginfo('Recieved data: %s Ang-Control: %s', data.x, ang_control)
     #Define PI Controller here
 
 def position_sub():
-    rospy.Subscriber('tracked_pos', Int32, callback) #Detect position from OpenCV
+    rospy.Subscriber('tracked_pos', location, callback) #Detect position from OpenCV
 
 def jackal_move():
+        global ang_control
         pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
         rospy.init_node('jackal_move', anonymous=True)
-        rate = rospy.Rate(50) # 1hz
+        rate = rospy.Rate(50) # 50hz
         position_sub()
+	linear = Vector3(0.0, 0.0, 0.0)
+	angular = Vector3(0.0, 0.0, ang_control)
+        message = Twist(linear, angular)
+	pub.publish(message)
+        #rospy.loginfo(message)
+        rate.sleep()
+
+if __name__ == '__main__':
+    while not rospy.is_shutdown():
+        try:
+            jackal_move()
+        except rospy.ROSInterruptException:
+            pass
